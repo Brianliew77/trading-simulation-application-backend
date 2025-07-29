6dee245097484d33a878eb0187ca356b9799adef
Brian Liew <brianlw1872@gmail.com>
2025-07-29 17:04:51 +0800

    add in all historical data

--- app/models.py
+++ app/models.py
@@ -73,4 +73,94 @@ class SimulatedWMT(Base):
     volume = Column(Integer)
 
 
+class GOOGHistorical(Base):
+    __tablename__ = "goog_2025_historical"
 
+    timestamp = Column(String, primary_key=True, index=True) 
+    open = Column(Float)
+    high = Column(Float)
+    low = Column(Float)
+    close = Column(Float)
+    adjusted_close = Column(Float)
+    volume = Column(Integer)
+    dividend_amount = Column(Float)
+    split_coefficient = Column(Float)
+
+class IBMHistorical(Base):
+    __tablename__ = "ibm_2025_historical"
+
+    timestamp = Column(String, primary_key=True, index=True) 
+    open = Column(Float)
+    high = Column(Float)
+    low = Column(Float)
+    close = Column(Float)
+    adjusted_close = Column(Float)
+    volume = Column(Integer)
+    dividend_amount = Column(Float)
+    split_coefficient = Column(Float)
+
+
+class MSFTHistorical(Base):
+    __tablename__ = "msft_2025_historical"
+
+    timestamp = Column(String, primary_key=True, index=True) 
+    open = Column(Float)
+    high = Column(Float)
+    low = Column(Float)
+    close = Column(Float)
+    adjusted_close = Column(Float)
+    volume = Column(Integer)
+    dividend_amount = Column(Float)
+    split_coefficient = Column(Float)
+
+class AAPLHistorical(Base):
+    __tablename__ = "simulated_aapl_2025_historical"
+
+    timestamp = Column(String, primary_key=True, index=True) 
+    open = Column(Float)
+    high = Column(Float)
+    low = Column(Float)
+    close = Column(Float)
+    adjusted_close = Column(Float)
+    volume = Column(Integer)
+    dividend_amount = Column(Float)
+    split_coefficient = Column(Float)
+
+class TSLAHistorical(Base):
+    __tablename__ = "tsla_2025_historical"
+
+    timestamp = Column(String, primary_key=True, index=True) 
+    open = Column(Float)
+    high = Column(Float)
+    low = Column(Float)
+    close = Column(Float)
+    adjusted_close = Column(Float)
+    volume = Column(Integer)
+    dividend_amount = Column(Float)
+    split_coefficient = Column(Float)
+
+class ULHistorical(Base):
+    __tablename__ = "ul_2025_historical"
+
+    timestamp = Column(String, primary_key=True, index=True) 
+    open = Column(Float)
+    high = Column(Float)
+    low = Column(Float)
+    close = Column(Float)
+    adjusted_close = Column(Float)
+    volume = Column(Integer)
+    dividend_amount = Column(Float)
+    split_coefficient = Column(Float)
+
+class WMTHistorical(Base):
+    __tablename__ = "wmt_2025_historical"
+
+    timestamp = Column(String, primary_key=True, index=True) 
+    open = Column(Float)
+    high = Column(Float)
+    low = Column(Float)
+    close = Column(Float)
+    adjusted_close = Column(Float)
+    volume = Column(Integer)
+    dividend_amount = Column(Float)
+    split_coefficient = Column(Float)
