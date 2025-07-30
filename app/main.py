5f219f3c0b9ac82114d95752746a50ce5d966f79
Brian Liew <brianlw1872@gmail.com>
2025-07-30 09:08:49 +0800

    added historical data to main amd models

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from sqlalchemy import text
 from app.database import SessionLocal
 from fastapi.middleware.cors import CORSMiddleware
 from app.models import SimulatedAAPL, SimulatedGOOG, SimulatedIBM, SimulatedMSFT, SimulatedTSLA, SimulatedUL, SimulatedWMT
+from app.models import GOOGHistorical, IBMHistorical, MSFTHistorical, AAPLHistorical,TSLAHistorical, ULHistorical, WMTHistorical
 from typing import List
 
 app = FastAPI()
@@ -37,12 +38,22 @@ def ping_db(db: Session = Depends(get_db)):
 @app.get("/stocks")
 def get_stocks(db: Session = Depends(get_db)):
     result = {
-        "aapl": db.query(SimulatedAAPL).limit(2).all(),
-        "goog": db.query(SimulatedGOOG).limit(2).all(),
-        "ibm": db.query(SimulatedIBM).limit(2).all(),
-        "msft": db.query(SimulatedMSFT).limit(2).all(),
-        "tsla": db.query(SimulatedTSLA).limit(2).all(),
-        "ul": db.query(SimulatedUL).limit(2).all(),
-        "wmt": db.query(SimulatedWMT).limit(2).all(),
+        # Live data
+        "aapl_live": db.query(SimulatedAAPL).limit(2).all(),
+        "goog_live": db.query(SimulatedGOOG).limit(2).all(),
+        "ibm_live": db.query(SimulatedIBM).limit(2).all(),
+        "msft_live": db.query(SimulatedMSFT).limit(2).all(),
+        "tsla_live": db.query(SimulatedTSLA).limit(2).all(),
+        "ul_live": db.query(SimulatedUL).limit(2).all(),
+        "wmt_live": db.query(SimulatedWMT).limit(2).all(),
+
+        # Historical data
+        "aapl_hist": db.query(AAPLHistorical).limit(2).all(),
+        "goog_hist": db.query(GOOGHistorical).limit(2).all(),
+        "ibm_hist": db.query(IBMHistorical).limit(2).all(),
+        "msft_hist": db.query(MSFTHistorical).limit(2).all(),
+        "tsla_hist": db.query(TSLAHistorical).limit(2).all(),
+        "ul_hist": db.query(ULHistorical).limit(2).all(),
+        "wmt_hist": db.query(WMTHistorical).limit(2).all(),
     }
     return result
